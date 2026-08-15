6d238e04b68d46ee147e24db1e3282abbd91cf86
Ryanhtech Labs <hello.ryanhtech.labs+git@gmail.com>
2026-08-15 13:37:35 +0200

    Inital commit

---
+++ src/DqApplication.py
@@ -0,0 +1,8 @@
+
+
+class DqApplication:
+    def __init__(self):
+        pass
+
+    def mainloop(self):
+        pass
---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""
+DrivingQuestions
+This is DrivingQuestions' main file.
+(c) 2026 Ryanhtech Labs
+"""
+from DqApplication import DqApplication
+
+def main():
+    _app = DqApplication()
+    _app.mainloop()
+
+if __name__ == "__main__":
+    main()
